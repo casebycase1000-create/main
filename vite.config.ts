ef2de7a4f6f12ece4d37b2fcaee5928ebbafec1e
casebycase1000-create <casebycase1000@gmail.com>
2026-08-12 20:28:12 -0400

    Fix Vite config to disable HMR

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,15 @@ import { fileURLToPath, URL } from 'node:url';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    hmr: false,
+    watch: {
+      usePolling: false,
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-
 });
